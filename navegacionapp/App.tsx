bf11522b06cc340f6e3ea98895608478a678caa4
Victor Asmad <victorasmad2@gmail.com>
2021-11-23 12:12:21 -0600

    FEAT - IDK

--- navegacionapp/App.tsx
+++ navegacionapp/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { View } from 'react-native'
+import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigator } from './src/navigator/StackNavigator';
 
 export const App = () => {
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
 
